5829b838d55d382927eb068753aa3a4d600e6228
Junyu-sev <90662397+Junyu-sev@users.noreply.github.com>
2024-06-26 08:47:45 +0800

    Add files via upload

---
+++ 4.Machine learning model/IHD_ProImp.py
@@ -0,0 +1,89 @@
+
+
+import numpy as np
+import pandas as pd
+from Utility.Training_Utilities import *
+from lightgbm import LGBMClassifier
+import warnings
+import re
+import shap
+pd.options.mode.chained_assignment = None  # default='warn'
+
+dpath = '/home/linmiao/ZhangjunYu/Proteomics_analysis/Data/'
+outpath = '/home/linmiao/ZhangjunYu/Proteomics_analysis/Result/'
+
+pro_df = pd.read_csv(dpath + 'Raw_data/231008_data_olink_instance_0.csv')
+pro_dict = pd.read_csv(dpath + 'Raw_data/protein_name.csv', usecols = ['Pro_code', 'Pro_definition'])
+
+target_df = pd.read_csv(dpath + 'Disease_outcomes/IHD_outcomes.csv', usecols = ['eid', 'target_y', 'BL2Target_yrs'])
+reg_df = pd.read_csv(dpath + 'Eid_info_data.csv', usecols = ['eid', 'Region_code'])
+
+mydf = pd.merge(target_df, pro_df, how = 'inner', left_on = ['eid'], right_on = ['ParticipantID1'])
+mydf = pd.merge(mydf, reg_df, how = 'left', on = ['eid'])
+
+pro_f_lst = pd.read_csv(outpath + 'cox/IHD.csv')
+pro_f_lst = pro_f_lst.loc[pro_f_lst.p_val_bfi<0.05].Pro_code.tolist() #原本文献中是p_val_bfi*3（因为有3种痴呆）
+
+my_params = {'n_estimators': 500,
+             'max_depth': 15,
+             'num_leaves': 10,
+             'subsample': 0.7,
+             'learning_rate': 0.01,
+             'colsample_bytree': 0.7}
+
+def normal_imp(mydict):
+    mysum = sum(mydict.values())
+    mykeys = mydict.keys()
+    for key in mykeys:
+        mydict[key] = mydict[key]/mysum
+    return mydict
+
+tg_imp_cv = Counter()
+tc_imp_cv = Counter()
+shap_imp_cv = np.zeros(len(pro_f_lst))
+fold_id_lst = [i for i in range(10)]
+
+for fold_id in fold_id_lst:
+    train_idx = mydf['Region_code'].index[mydf['Region_code'] != fold_id]
+    test_idx = mydf['Region_code'].index[mydf['Region_code'] == fold_id]
+    X_train, X_test = mydf.iloc[train_idx][pro_f_lst], mydf.iloc[test_idx][pro_f_lst]
+    y_train, y_test = mydf.iloc[train_idx].target_y, mydf.iloc[test_idx].target_y
+    my_lgb = LGBMClassifier(objective = 'binary', metric = 'auc', is_unbalance = True, verbosity = 1, seed = 2023)
+    my_lgb.set_params(**my_params)
+    my_lgb.fit(X_train, y_train)
+    totalgain_imp = my_lgb.booster_.feature_importance(importance_type='gain')
+    totalgain_imp = dict(zip(my_lgb.booster_.feature_name(), totalgain_imp.tolist()))
+    totalcover_imp = my_lgb.booster_.feature_importance(importance_type='split')
+    totalcover_imp = dict(zip(my_lgb.booster_.feature_name(), totalcover_imp.tolist()))
+    tg_imp_cv += Counter(normal_imp(totalgain_imp))
+    tc_imp_cv += Counter(normal_imp(totalcover_imp))
+    explainer = shap.TreeExplainer(my_lgb)
+    shap_values = explainer.shap_values(X_test)
+    shap_values = np.abs(np.average(shap_values, axis=0))
+    shap_imp_cv += shap_values / np.sum(shap_values)
+
+
+shap_imp_df = pd.DataFrame({'Pro_code': pro_f_lst,
+                            'ShapValues_cv': shap_imp_cv/10})
+shap_imp_df.sort_values(by = 'ShapValues_cv', ascending = False, inplace = True)
+
+tg_imp_cv = normal_imp(tg_imp_cv)
+tg_imp_df = pd.DataFrame({'Pro_code': list(tg_imp_cv.keys()),
+                          'TotalGain_cv': list(tg_imp_cv.values())})
+
+tc_imp_cv = normal_imp(tc_imp_cv)
+tc_imp_df = pd.DataFrame({'Pro_code': list(tc_imp_cv.keys()),
+                          'TotalCover_cv': list(tc_imp_cv.values())})
+
+my_imp_df = pd.merge(left = shap_imp_df, right = tg_imp_df, how = 'left', on = ['Pro_code'])
+my_imp_df = pd.merge(left = my_imp_df, right = tc_imp_df, how = 'left', on = ['Pro_code'])
+my_imp_df['Ensemble'] = (my_imp_df['ShapValues_cv'] + my_imp_df['TotalGain_cv'] + my_imp_df['TotalCover_cv'])/3
+my_imp_df.sort_values(by = 'Ensemble', ascending = False, inplace = True)
+my_imp_df = pd.merge(my_imp_df, pro_dict, how = 'left', on=['Pro_code'])
+
+my_imp_df.to_csv(outpath + 'LightGBM/IHD/ProImportance.csv', index = False)
+
+print('finished')
+
+
+
